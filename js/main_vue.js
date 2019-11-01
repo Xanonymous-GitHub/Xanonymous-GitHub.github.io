768a47383303ffa750312fa08f458d716243266c
Xanonymous <trusaidlin@gmail.com>
2019-11-02 00:00:11 +0800

    pic

---
+++ js/main_vue.js
@@ -0,0 +1,12 @@
+const left = new Vue({
+    el: '.left',
+    data: {
+        my_status: 'Learning EVERYTHING.'
+    }
+});
+const right = new Vue({
+    el: '.right',
+    data: {
+        title: 'Xanonymous'
+    }
+})
